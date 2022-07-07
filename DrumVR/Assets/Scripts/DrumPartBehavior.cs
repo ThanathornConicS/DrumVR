3a000bfb3ef3c8bcaa3068559a114fb6587bf4ca
Akira <akira.rds01@gmail.com>
2022-07-07 13:55:24 +0700

    Directional Collision Check

--- DrumVR/Assets/Scripts/DrumPartBehavior.cs
+++ DrumVR/Assets/Scripts/DrumPartBehavior.cs
@@ -6,6 +6,8 @@ using UnityEngine.XR.Interaction.Toolkit;
 [RequireComponent(typeof(AudioSource))]
 public class DrumPartBehavior : MonoBehaviour
 {
+    public float hitAngle = 90f;
+
     AudioSource m_audioSource;
 
     void Start()
@@ -17,9 +19,14 @@ public class DrumPartBehavior : MonoBehaviour
     {
         if(collider.gameObject.layer == LayerMask.NameToLayer("Stick"))
         {
-            VibrationManager.Instance.TriggerVibration(m_audioSource.clip, collider.gameObject);
-            m_audioSource.pitch = Random.Range(0.9f, 1.1f);
-            m_audioSource.Play();
+            ControllerVelocity controllerVelocity = collider.gameObject.transform.parent.gameObject.GetComponent<ControllerVelocity>();
+            float angle = Vector3.Angle(Vector3.down, controllerVelocity.Velocity);
+            
+            if(angle < hitAngle){
+                VibrationManager.Instance.TriggerVibration(m_audioSource.clip, collider.gameObject);
+                m_audioSource.pitch = Random.Range(0.95f, 1.05f);
+                m_audioSource.Play();
+            }
         }
     }
 }
