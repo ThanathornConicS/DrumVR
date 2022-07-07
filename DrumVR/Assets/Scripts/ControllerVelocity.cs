bdc2753740f165a0ef36b53af3f5256e0bc26bca
Thanathorn Nilkhao <thanathorn@outlook.com>
2022-07-07 11:59:47 +0700

    Controller Velocity

---
+++ DrumVR/Assets/Scripts/ControllerVelocity.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class ControllerVelocity : MonoBehaviour
+{
+    public InputActionProperty velocityProperty;
+    public Vector3 Velocity {get; private set;} = Vector3.zero;
+
+    public TMP_Text velText;
+
+    [SerializeField]
+    private float velocityMagnitude = 0.0f;
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    private void Update()
+    {
+        Velocity = velocityProperty.action.ReadValue<Vector3>();
+        velocityMagnitude = Velocity.magnitude;
+
+        Debug.Log("Magnitude: " + Velocity);
+        velText.text = "Velocity" + Velocity + " with magnitude: " + velocityMagnitude;
+    }
+}
--- DrumVR/Assets/Scripts/DrumPartBehavior.cs
+++ DrumVR/Assets/Scripts/DrumPartBehavior.cs
@@ -2,22 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class DrumPartBehavior : MonoBehaviour
 {
+    AudioSource m_audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    void OnTriggerEnter()
-    {
-        Debug.Log("Drum Sound");
+        Debug.Log("Entered");
+        m_audioSource.Play();
     }
 }
