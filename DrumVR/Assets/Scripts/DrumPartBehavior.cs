40db96cd4e53cdd03577e9ea0a5e810309cc7ee5
Akira <akira.rds01@gmail.com>
2022-07-07 11:54:57 +0700

    Haptic feedback

--- DrumVR/Assets/Scripts/DrumPartBehavior.cs
+++ DrumVR/Assets/Scripts/DrumPartBehavior.cs
@@ -1,23 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
+[RequireComponent(typeof(AudioSource))]
 public class DrumPartBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    AudioSource m_audioSource;
+
     void Start()
     {
-        
+        m_audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider collider)
     {
-        
-    }
-
-    void OnTriggerEnter()
-    {
-        Debug.Log("Drum Sound");
+        if(collider.gameObject.layer == LayerMask.NameToLayer("Stick"))
+        {
+            VibrationManager.Instance.TriggerVibration(m_audioSource.clip, collider.gameObject);
+            m_audioSource.Play();
+        }
     }
 }
