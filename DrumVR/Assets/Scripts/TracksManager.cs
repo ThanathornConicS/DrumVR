779a03c0795ab9e9bdeb17742e201ceaf36df2a2
Thanathorn Nilkhao <thanathorn@outlook.com>
2022-07-11 13:58:28 +0700

    Audio Player

---
+++ DrumVR/Assets/Scripts/TracksManager.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(AudioSource))]
+public class TracksManager : MonoBehaviour
+{
+    public AudioClip[] clips;
+
+    [SerializeField]
+    private int currentTrack;
+    private AudioSource source;
+
+    public TMP_Text clipTitleText;
+    public TMP_Text clipTimeText;
+
+    private int fullLength;
+    private int playTime;
+    private int seconds;
+    private int minutes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        currentTrack = 0;
+
+        PlayTrack();
+    }
+
+    public void PlayTrack()
+    {
+        if(source.isPlaying)
+        {
+            return;
+        }
+
+        currentTrack--;
+        if(currentTrack < 0)
+        {
+            currentTrack = clips.Length - 1;
+        }
+        StartCoroutine("WaitForTrackEnd");
+    }
+    public void StopTrack()
+    {
+        StopCoroutine("WaitForTrackEnd");
+        source.Stop();
+    }
+
+    IEnumerator WaitForTrackEnd()
+    {
+        while(source.isPlaying)
+        {
+            playTime = (int)source.time;
+            ShowPlayTime();
+            yield return null;
+        }
+        NextTrack();
+    }
+
+    public void NextTrack()
+    {
+        source.Stop();
+        currentTrack++;
+        if(currentTrack > clips.Length - 1)
+        {
+            currentTrack = 0;
+        }
+
+        source.clip = clips[currentTrack];
+        source.Play();
+
+        ShowCurrentTitle();
+
+        StartCoroutine("WaitForTrackEnd");
+    }
+    public void PreviousTrack()
+    {
+        source.Stop();
+        currentTrack--;
+        if(currentTrack < 0)
+        {
+            currentTrack = 0;
+        }
+
+        source.clip = clips[currentTrack];
+        source.Play();
+
+        StartCoroutine("WaitForTrackEnd");
+    }
+    public void MuteTrack()
+    {
+        source.mute = !source.mute;
+    }
+
+    private void ShowCurrentTitle()
+    {
+        clipTitleText.text = source.clip.name;
+        fullLength = (int)source.clip.length;
+    }
+    private void ShowPlayTime()
+    {
+        seconds = playTime % 60;
+        minutes = (playTime / 60) % 60;
+
+        clipTimeText.text = minutes + ":" + seconds.ToString("D2") + "/";
+    }
+}
